10e52bdde376dba1c4aaa63c2e30056d02f2a22e
KeshavAgrawal <KeshavAgrawal@AAILT-00330>
2025-05-18 11:21:14 +0530

    Add project files.

---
+++ Assessment_First_Job/Data/AppDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Assessment_First_Job.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assessment_First_Job.Data
+{
+    public class AppDBContext(DbContextOptions<AppDBContext>options) : DbContext(options)
+    {
+        public DbSet<UserDetails> UserDetails {  get; set; }
+    }
+}
---
+++ Assessment_First_Job/Model/UserDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assessment_First_Job.Model
+{
+    public class UserDetails
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(25)]
+        public string? Title { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string? Description { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
